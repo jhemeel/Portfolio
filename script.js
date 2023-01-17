2de7463151a17c8af04e6426d62cf38a56fe43f9
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-01-17 13:13:28 +0100

    contact me section added  made responsive for mobile users

--- script.js
+++ script.js
@@ -28,3 +28,72 @@ mode.addEventListener('click', ()=>{
 
     }
 })
+
+
+const FloatLabel = (() => {
+
+  
+  // add active class
+  const handleFocus = (e) => {
+    const target = e.target;
+    target.parentNode.classList.add('active');
+    target.setAttribute('placeholder', target.getAttribute('data-placeholder'));
+  };
+  
+  // remove active class
+  const handleBlur = (e) => {
+    const target = e.target;
+    if(!target.value) {
+      target.parentNode.classList.remove('active');
+    }
+    target.removeAttribute('placeholder');
+  };
+  
+  // register events
+  const bindEvents = (element) => {
+    const floatField = element.querySelector('input');
+    floatField.addEventListener('focus', handleFocus);
+    floatField.addEventListener('blur', handleBlur);    
+  };
+  
+  // get DOM elements
+  const init = () => {
+    const floatContainers = document.querySelectorAll('.floatedForm');
+
+    floatContainers.forEach((element) => {
+
+      if (element.querySelector('input').value) {
+        element.classList.add('active');
+      }
+
+      bindEvents(element);
+    });
+  };
+  
+  return {
+    init: init
+  };
+})();
+
+FloatLabel.init();
+
+
+// hamburger
+
+let hamburger = document.querySelector('.hamburger')
+let navLink = document.querySelector('.nav-link')
+let navList = document.querySelector('.nav-list')
+
+hamburger.addEventListener('click', ()=>{
+  hamburger.classList.toggle('active')
+  navList.classList.toggle('active')
+})
+
+
+navLink.addEventListener('click', ()=>{
+  hamburger.classList.remove('active')
+  navList.classList.remove('active')
+})
+
+
+// scrollToview
